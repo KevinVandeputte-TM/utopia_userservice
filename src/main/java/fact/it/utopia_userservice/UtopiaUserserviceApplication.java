28684a7937894b89ea0c9e5693d47d608d28b4d5
Kevin Vandeputte <r0792871@student.thomasmore.be>
2022-12-29 17:50:31 +0100

    new project

---
+++ src/main/java/fact/it/utopia_userservice/UtopiaUserserviceApplication.java
@@ -0,0 +1,13 @@
+package fact.it.utopia_userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaUserserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UtopiaUserserviceApplication.class, args);
+    }
+
+}
